89b90a6d29a814f1841e4c740d2db6290c7170f6
andre mendes <andre.eletrom@hotmail.com>
2020-09-03 00:45:36 -0300

    Validacao customizada de CPF ou CNPJ na insercao de cliente

--- src/main/java/com/aulas/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/aulas/cursomc/dto/ClienteNewDTO.java
@@ -1,31 +1,43 @@
 package com.aulas.cursomc.dto;
 
+import com.aulas.cursomc.services.validation.ClienteInsert;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
+@ClienteInsert //anotacao customizada, para validar cpf ou cnpj
 public class ClienteNewDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     @Getter
     @Setter
+    @NotEmpty(message = "Preenchimento obrigatório")
+    @Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres")
     private String nome;
     @Getter
     @Setter
+    @NotEmpty(message = "Preenchimento obrigatório")
+    @Email(message = "Email invalido")
     private String email;
     @Getter
     @Setter
+    @NotEmpty(message = "Preenchimento obrigatório")
     private String cpfOuCnpj;
     @Getter
-    @Setter //verificar
+    @Setter
     private Integer tipo;
     @Getter
     @Setter
+    @NotEmpty(message = "Preenchimento obrigatório")
     private String logradouro;
     @Getter
     @Setter
+    @NotEmpty(message = "Preenchimento obrigatório")
     private String numero;
     @Getter
     @Setter
@@ -35,9 +47,11 @@ public class ClienteNewDTO implements Serializable {
     private String bairro;
     @Getter
     @Setter
+    @NotEmpty(message = "Preenchimento obrigatório")
     private String cep;
     @Getter
     @Setter
+    @NotEmpty(message = "Preenchimento obrigatório")
     private String telefone1;
     @Getter
     @Setter
