c976315e998f3affea2bc0ff3e0237d197a2bbc8
andre mendes <andre.eletrom@hotmail.com>
2020-09-27 23:29:57 -0300

    Implementando autenticacao e geracao de token JWT

---
+++ src/main/java/com/aulas/cursomc/dto/CredenciaisDTO.java
@@ -0,0 +1,22 @@
+package com.aulas.cursomc.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+public class CredenciaisDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Getter
+    @Setter
+    private String email;
+    @Getter
+    @Setter
+    private String senha;
+
+    public CredenciaisDTO() {
+
+    }
+}
